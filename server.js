e1d0c1659ca559d328177fa04785dd36231db0bc
gus616 <guzz.vc@gmail.com>
2020-10-12 17:56:35 -0500

    setup de rutas

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 
 //Load env vars
@@ -7,6 +11,16 @@ dotenv.config({path:'./config/config.env'});
 
 
 const app = express();
+
+//Middleware 
+app.use(logger);
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
