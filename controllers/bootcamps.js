c0a6a9121b59ebbbe20a4c6fc2c80e64dda9f37b
gus616 <guzz.vc@gmail.com>
2020-10-13 13:38:21 -0500

    se agrego esquema mongoose y conexion a mongoDB

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const Bootcamp = require('../models/Bootcamp');
 
 //@desc    Get All bootcamps
 //@route   GET /api/v1/bootcamps
@@ -18,9 +19,22 @@ exports.getBootcamp = (req, res, next) =>{
 //@desc    Get new bootcamp
 //@route   POST /api/v1/bootcamps
 //@access  Private
-exports.createBootcamp = (req, res, next) =>{
-    res.status(200).json({success: true, msg: 'Create new bootcamp'});
-}
+exports.createBootcamp = async (req, res, next) =>{
+    console.log(req.body);
+   // res.status(200).json({success: true, msg: 'Create new bootcamp'});
+   try{
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+   } catch(err){
+       res.status(400).json({
+           success: false,
+           error: err
+       });
+   }    
+};
 
 
 //@desc    Update bootcamp
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
 
-//Load env vars
-dotenv.config({path:'./config/config.env'});
 
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 //Middleware 
-app.use(logger);
+
+//Dev loggin middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+//app.use(logger);
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -21,6 +36,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} ...`.yellow.bold));
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) =>{
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
 
